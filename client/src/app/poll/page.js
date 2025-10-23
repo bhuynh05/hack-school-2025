f042c20976572c14825b382f4defbc56c57ae67a
Brandon Huynh <brandon.huynh05@gmail.com>
2025-10-22 18:41:33 -0700

    Finished workshop 1

---
+++ client/src/app/poll/page.js
@@ -0,0 +1,4 @@
+export default function PollPage({params}){
+    const slug = params
+    return <h1> This poll's slug is: {slug}</h1>;
+}
--- client/src/components/PollPreview.js
+++ client/src/components/PollPreview.js
@@ -1,17 +1,18 @@
 "use client";
 import Link from "next/link";
 import styles from "./PollPreview.module.css";
-
 /* #1a TODO: Add title, totalVotes, and ownerId to the props list*/
-export default function PollPreview({ link }) {
-  return (
+export default function PollPreview({ link, title, totalVotes, ownerID}) {
+  return ( 
     <Link href={link} className={styles.PollLink}>
       <div className={styles.infoContainer}>
         {/* #1b TODO: In the line below, add an expression containing the title between the h2 tags*/}
-        <h2 className={styles.pollTitle}> </h2>
+        <h2 className={styles.pollTitle}> {title} </h2>
         {/* #1c TODO: On a new line, add a <p> tag containing the totalVotes to the component*/}
+        <p>{totalVotes} votes</p>
       </div>
       {/* #1d TODO: On a new line, add a <p> tag containing the ownerId to the component*/}
+      <p className={styles.ownerId}> {ownerID} </p>
     </Link>
   );
 }
